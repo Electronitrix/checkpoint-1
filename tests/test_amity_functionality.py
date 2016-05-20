c2f15e169ab0dbc57b74f942d09dd45a33145c24
Chukwuerika Dike <chukwuerika.dike@andela.com>
2016-05-21 00:22:21 +0100

    [Chore] Abstracted duplicated code into two separate functions

--- tests/test_amity_functionality.py
+++ tests/test_amity_functionality.py
@@ -136,21 +136,21 @@ class AmityRoomAllocationFunctionalityTestSuite(unittest.TestCase):
     def test_print_person_identifier(self):
         main.create_room("bellows", 0, "office")
         main.add_person("erika", "dike", "staff")
-        appOutput = ("ERIKA DIKE's identifier: 0\n")
-        with capture(main.print_person_id, "erika","dike") as output:
+        test_output = ("ERIKA DIKE's identifier: 0\n")
+        with capture(main.print_person_id, "erika", "dike") as app_output:
             self.assertEqual(
-                appOutput, output,
+                test_output, app_output,
                 msg=("get_person_identifier does not print as expected!!! "
-                     "{0} != {1}").format(appOutput, output)
+                     "{0} != {1}").format(test_output, app_output)
             )
 
     def test_print_invalid_person_identifier(self):
         main.add_person("erika", "dike", "staff")
-        appOutput = ("No identifier was found for the name supplied\n")
-        with capture(main.print_person_id, "ina","dike") as output:
+        test_output = ("No identifier was found for the name supplied\n")
+        with capture(main.print_person_id, "ina", "dike") as app_output:
             self.assertEqual(
-                appOutput, output,
-                msg=("{0} != {1}").format(appOutput, output)
+                test_output, app_output,
+                msg=("{0} != {1}").format(test_output, app_output)
             )
 
     def test_graceful_handling_when_adding_person_and_no_room_exists(self):
@@ -185,24 +185,24 @@ class AmityRoomAllocationFunctionalityTestSuite(unittest.TestCase):
         main.create_room("bellows", 0, "office")
         main.add_person("erika", "dike", "staff")
         main.create_room("iroko", 1, "living space")
-        appOutput = "You cannot allocate staff to 'living space'.\n"
-        with capture(main.reallocate_person, 0, "iroko") as output:
+        test_output = "You cannot allocate staff to 'living space'.\n"
+        with capture(main.reallocate_person, 0, "iroko") as app_output:
             self.assertEqual(
-                appOutput, output,
+                test_output, app_output,
                 msg=("Staff cannot be reallocated to living space!!! {0} != "
-                     "{1}".format(appOutput, output))
+                     "{1}".format(test_output, app_output))
             )
 
     def test_invalid_person_identifiers_arguments_to_reallocate_person(self):
         main.create_room("bellows", 0, "office")
         main.add_person("erika", "dike", "staff")
         main.create_room("bench hook", 0, "office")
-        appOutput = ("No person was found in memory for person identifier "
-                     "supplied.\n")
-        with capture(main.reallocate_person, 45, "bench hook") as output:
+        test_output = ("No person was found in memory for person identifier "
+                       "supplied.\n")
+        with capture(main.reallocate_person, 45, "bench hook") as app_output:
             self.assertEqual(
-                appOutput, output,
-                msg="{} != {}".format(appOutput, output)
+                test_output, app_output,
+                msg="{} != {}".format(test_output, app_output)
             )
 
     def test_does_not_allocate_to_filled_living_space(self):
@@ -212,15 +212,15 @@ class AmityRoomAllocationFunctionalityTestSuite(unittest.TestCase):
         main.add_person("sunday", "nwuguru", "fellow", "Y")
         main.add_person("stephen", "oduntan", "fellow", "Y")
         main.add_person("seyi", "adekoya", "fellow", "Y")
-        appOutput = ("No room was found in memory with that name. You may "
-                     "want to create a new room with that\n"
-                     "name or view all rooms in memory by requesting "
-                     "allocations.\n"
-                     )
-        with capture(main.reallocate_person, 4, "iroko") as output:
+        test_output = ("No room was found in memory with that name. You may "
+                       "want to create a new room with that\n"
+                       "name or view all rooms in memory by requesting "
+                       "allocations.\n"
+                       )
+        with capture(main.reallocate_person, 4, "iroko") as app_output:
             self.assertEqual(
-                appOutput, output,
-                msg=("{} != {}".format(appOutput, output))
+                test_output, app_output,
+                msg=("{} != {}".format(test_output, app_output))
             )
 
     def test_loads_people(self):
@@ -250,63 +250,39 @@ class AmityRoomAllocationFunctionalityTestSuite(unittest.TestCase):
         )
 
     def test_print_allocations_when_there_are_allocations(self):
-        main.create_room("iroko", 0, "living space")
-        main.create_room("bellows", 0, "office")
-        main.add_person("erika", "dike", "fellow", "Y")
-        main.add_person("eze", "janu", "fellow", "Y")
-        main.add_person("sunday", "nwuguru", "fellow", "Y")
-        main.add_person("stephen", "oduntan", "fellow", "Y")
-        main.add_person("nengi", "adoki", "staff")
-
-        appOutput = []
-        temp = []
-        line_width = 51
-
-        temp.append("BELLOWS\n")
-        temp.append("-" * line_width)
-        temp.append("\nERIKA DIKE, EZE JANU, SUNDAY NWUGURU, ")
-        temp.append("STEPHEN ODUNTAN, NENGI ADOKI")
-        appOutput.append(textwrap.fill("".join(temp), line_width))
-        appOutput.append("\n\n")
-
-        temp = []
-        temp.append("IROKO\n")
-        temp.append("-" * line_width)
-        temp.append("\nERIKA DIKE, EZE JANU, SUNDAY NWUGURU, "
-                    "STEPHEN ODUNTAN\n\n\n")
-        appOutput.append(textwrap.fill("".join(temp), line_width))
-        appOutput.append("\n\n\n")
-        appOutput = "".join(appOutput)
-
-        with capture(main.print_allocations, "screen") as \
-                output:
+        self.add_data_to_memory_for_print_allocation_tests()
+        test_output = self.prepare_test_output()
+        test_output += "\n"
+        with capture(main.print_allocations, "screen") as app_output:
             self.assertEqual(
-                appOutput, output,
-                msg=("{0} != {1}!!!".format(appOutput, output))
-            )
-
-    def test_print_allocations_when_there_are_no_allocations(self):
-        appOutput = ("There are no allocations in memory. You can either "
-                     "enter new data or load data from database\n")
-        with capture(main.print_allocations, "screen") as output:
-            self.assertEqual(
-                appOutput, output,
-                msg=("- {0} != {1}!!!".format(appOutput, output))
+                test_output, app_output,
+                msg=("{0} != {1}!!!".format(test_output, app_output))
             )
 
     def test_print_allocations_to_file(self):
-        main.create_room("iroko", 0, "living space")
-        main.create_room("bellows", 0, "office")
-        main.add_person("erika", "dike", "fellow", "Y")
-        main.add_person("eze", "janu", "fellow", "Y")
-        main.add_person("sunday", "nwuguru", "fellow", "Y")
-        main.add_person("stephen", "oduntan", "fellow", "Y")
-        main.add_person("nengi", "adoki", "staff")
+        self.add_data_to_memory_for_print_allocation_tests()
         main.print_allocations(self.alloc_txt_file)
         with open(self.alloc_txt_file, "r") as file:
             allocations = file.read()
 
-        appOutput = []
+        test_output = self.prepare_test_output()
+
+        self.assertEqual(
+            test_output, allocations,
+            msg=("{0} != {1}!!!".format(test_output, allocations))
+        )
+
+    def add_data_to_memory_for_print_allocation_tests(self):
+        main.create_room("iroko", 0, "living space")
+        main.create_room("bellows", 0, "office")
+        main.add_person("erika", "dike", "fellow", "Y")
+        main.add_person("eze", "janu", "fellow", "Y")
+        main.add_person("sunday", "nwuguru", "fellow", "Y")
+        main.add_person("stephen", "oduntan", "fellow", "Y")
+        main.add_person("nengi", "adoki", "staff")
+
+    def prepare_test_output(self):
+        app_output = []
         temp = []
         line_width = 51
 
@@ -314,90 +290,95 @@ class AmityRoomAllocationFunctionalityTestSuite(unittest.TestCase):
         temp.append("-" * line_width)
         temp.append("\nERIKA DIKE, EZE JANU, SUNDAY NWUGURU, ")
         temp.append("STEPHEN ODUNTAN, NENGI ADOKI")
-        appOutput.append(textwrap.fill("".join(temp), line_width))
-        appOutput.append("\n\n")
+        app_output.append(textwrap.fill("".join(temp), line_width))
+        app_output.append("\n\n")
 
         temp = []
         temp.append("IROKO\n")
         temp.append("-" * line_width)
         temp.append("\nERIKA DIKE, EZE JANU, SUNDAY NWUGURU, "
                     "STEPHEN ODUNTAN\n\n\n")
-        appOutput.append(textwrap.fill("".join(temp), line_width))
-        appOutput.append("\n\n")
-        appOutput = "".join(appOutput)
+        app_output.append(textwrap.fill("".join(temp), line_width))
+        app_output.append("\n\n")
+        return "".join(app_output)
 
-        self.assertEqual(
-            appOutput, allocations,
-            msg=("{0} != {1}!!!".format(appOutput, allocations))
-        )
+    def test_print_allocations_when_there_are_no_allocations(self):
+        test_output = ("There are no allocations in memory. You can either "
+                       "enter new data or load data from database\n")
+        with capture(main.print_allocations, "screen") as app_output:
+            self.assertEqual(
+                test_output, app_output,
+                msg=("- {0} != {1}!!!".format(test_output, app_output))
+            )
 
     def test_print_allocations_invalid_file(self):
         main.create_room("bellows", 0, "office")
         main.add_person("erika", "dike", "fellow", "Y")
-        appOutput = ("You must enter a file name with a .txt extension to save"
-                     " to file\n")
-        with capture(main.print_allocations, "alloc.pkl") as output:
+        test_output = ("You must enter a file name with a .txt extension to "
+                       "save to file\n")
+        with capture(main.print_allocations, "alloc.pkl") as app_output:
             self.assertEqual(
-                appOutput, output,
+                test_output, app_output,
                 msg=("print_allocations does not detect invalid file "
-                     "extensions - {0} != {1}".format(appOutput, output))
+                     "extensions - {0} != {1}".format(test_output, app_output))
             )
 
     def test_print_unallocated(self):
         main.add_person("erika", "dike", "fellow", "Y")
-        appOutput = ("LIST OF ALL UNALLOCATED PEOPLE\n\n"
-                     "ERIKA DIKE\n\n")
-        with capture(main.print_unallocated, "screen") as output:
+        test_output = ("LIST OF ALL UNALLOCATED PEOPLE\n\n"
+                       "ERIKA DIKE\n\n")
+        with capture(main.print_unallocated, "screen") as app_output:
             self.assertEqual(
-                appOutput, output,
-                msg=("{0} != {1}!!!".format(appOutput, output))
+                test_output, app_output,
+                msg=("{0} != {1}!!!".format(test_output, app_output))
             )
 
     def test_print_unallocated_to_file(self):
         main.add_person("erika", "dike", "fellow", "Y")
         main.print_unallocated(self.unalloc_txt_file)
+
         with open(self.unalloc_txt_file, "r") as file:
             unallocated = file.read()
-        appOutput = ("LIST OF ALL UNALLOCATED PEOPLE\n\n"
-                     "ERIKA DIKE\n")
+
+        test_output = ("LIST OF ALL UNALLOCATED PEOPLE\n\nERIKA DIKE\n")
         self.assertEqual(
-            appOutput, unallocated,
-            msg=("{0} != {1}".format(appOutput, unallocated))
+            test_output, unallocated,
+            msg=("{0} != {1}".format(test_output, unallocated))
         )
 
     def test_print_unallocated_invalid_file(self):
         main.add_person("erika", "dike", "fellow", "Y")
-        appOutput = ("You must enter a file name with a .txt extension to save"
-                     " to file\n")
-        with capture(main.print_unallocated, "unalloc.pkl") as output:
+        test_output = ("You must enter a file name with a .txt extension to "
+                       "save to file\n")
+
+        with capture(main.print_unallocated, "unalloc.pkl") as app_output:
             self.assertEqual(
-                appOutput, output,
-                msg=("{0} != {1}".format(appOutput, output))
+                test_output, app_output,
+                msg=("{0} != {1}".format(test_output, app_output))
             )
 
     def test_print_room_with_occupants(self):
         main.create_room("bellows", 0, "office")
         main.add_person("erika", "dike", "fellow", "Y")
-        appOutput = ("NAMES OF PEOPLE IN BELLOWS\n\n"
-                     "ERIKA DIKE\n\n\n"
-                     )
-        with capture(main.print_room, "bellows") as output:
+        test_output = ("NAMES OF PEOPLE IN BELLOWS\n\nERIKA DIKE\n\n\n")
+
+        with capture(main.print_room, "bellows") as app_output:
             self.assertEqual(
-                appOutput, output,
+                test_output, app_output,
                 msg=("print_room does not print correct room content "
-                     "- {0} != {1}!!!".format(appOutput, output))
+                     "- {0} != {1}!!!".format(test_output, app_output))
             )
 
     def test_print_room_with_out_occupants(self):
         main.create_room("bellows", 0, "office")
-        appOutput = ("NAMES OF PEOPLE IN BELLOWS\n\n"
-                     "There is no one in this Room!!!\n\n"
-                     )
-        with capture(main.print_room, "bellows") as output:
+        test_output = ("NAMES OF PEOPLE IN BELLOWS\n\n"
+                       "There is no one in this Room!!!\n\n"
+                       )
+        with capture(main.print_room, "bellows") as app_output:
             self.assertEqual(
-                appOutput, output,
+                test_output, app_output,
                 msg=("print_room does not print correct room content "
-                     "- {0} != {1}!!!".format(appOutput, output))
+                     "- {0} != {1}!!!".format(test_output, app_output))
             )
 
     def tearDown(self):
